faeae939feecd32a4c3ed3b6d321992d07c42af6
Prometheus1994 <48414632+Prometheus1994@users.noreply.github.com>
2025-10-01 14:08:22 +0100

    Update rip (using power_of_2).c

--- rip/rip (using power_of_2).c
+++ rip/rip (using power_of_2).c
@@ -39,57 +39,42 @@ int is_balanced(char *str)
 	return 0;
 }
 
-int main(int argc, char **argv)
+void inside(char *argv1, int *min, int step)
 {
-	char array[ft_strlen(argv[1]) + 1];
-	array[ft_strlen(argv[1])] = '\0';
-	int min = ft_strlen(argv[1]) / 2 + 1;
+	int len = ft_strlen(argv1);
+	char array[len + 1];
+	array[len] = '\0';
 	int i = 0;
-	while (i < power_of_2(ft_strlen(argv[1]) / 2 + 1))
+	while (i < power_of_2(len))
 	{
+		int spaces_nbr = 0;
 		int j = 0;
-		while (argv[1][j] != '\0')
+		while (argv1[j] != '\0')
 		{
-			array[j] = argv[1][j];
+			array[j] = argv1[j];
 			j++;
 		}
-		int underscore_nbr = 0;
 		j = 0;
-		while (j < ft_strlen(argv[1]) / 2 + 1)
+		while (j < len)
 		{
 			if (i / power_of_2(j) % 2)
 			{
-				array[j * 2] = '_';
-				underscore_nbr++;
+				array[j] = ' ';
+				spaces_nbr++;
 			}
 			j++;
 		}
-		if (is_balanced(array) && underscore_nbr < min)
-			min = underscore_nbr;
-		i++;
-	}
-	i = 0;
-	while (i < power_of_2(ft_strlen(argv[1]) / 2 + 1))
-	{
-		int j = 0;
-		while (argv[1][j] != '\0')
-		{
-			array[j] = argv[1][j];
-			j++;
-		}
-		int underscore_nbr = 0;
-		j = 0;
-		while (j < ft_strlen(argv[1]) / 2 + 1)
-		{
-			if (i / power_of_2(j) % 2)
-			{
-				array[j * 2] = '_';
-				underscore_nbr++;
-			}
-			j++;
-		}
-		if (is_balanced(array) && underscore_nbr == min)
+		if (step == 1 && is_balanced(array) && spaces_nbr < *min)
+			*min = spaces_nbr;
+		if (step == 2 && is_balanced(array) && spaces_nbr == *min)
 			puts(array);
 		i++;
 	}
+}
+
+int main(int argc, char **argv)
+{
+	int min = ft_strlen(argv[1]);
+	inside(argv[1], &min, 1);
+	inside(argv[1], &min, 2);
 }
